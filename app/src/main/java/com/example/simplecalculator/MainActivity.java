0b776c3bb478a2f29937806aacd6804a20d54a8c
Khadafi Rimbang <khadafirimbang7@gmail.com>
2023-01-15 06:27:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,243 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button zero, one, two, three, four, five, six, seven, eight, nine, modulo, clear, del, div, mul, sub, add, equal, dot, root;
+    TextView tvTop, tvBottom;
+    double firstNum, secondNum, result;
+    char op;
+    boolean rooted = false;
+    String printResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTop = findViewById(R.id.tvTop);
+        tvBottom = findViewById(R.id.tvBottom);
+        zero = findViewById(R.id.zero);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        five = findViewById(R.id.five);
+        six = findViewById(R.id.six);
+        seven = findViewById(R.id.seven);
+        eight = findViewById(R.id.eight);
+        nine = findViewById(R.id.nine);
+        modulo = findViewById(R.id.modulo);
+        clear = findViewById(R.id.clear);
+        del = findViewById(R.id.del);
+        div = findViewById(R.id.div);
+        mul = findViewById(R.id.mul);
+        sub = findViewById(R.id.sub);
+        add = findViewById(R.id.add);
+        equal = findViewById(R.id.equal);
+        dot = findViewById(R.id.dot);
+        root = findViewById(R.id.root);
+
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstNum = Double.parseDouble(tvBottom.getText().toString());
+                op = '+';
+                tvTop.setText(String.valueOf(firstNum) + op);
+                tvBottom.setText("");
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstNum = Double.parseDouble(tvBottom.getText().toString());
+                op = '-';
+                tvTop.setText(String.valueOf(firstNum) + op);
+                tvBottom.setText("");
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstNum = Double.parseDouble(tvBottom.getText().toString());
+                op = '*';
+                tvTop.setText(String.valueOf(firstNum) + 'x');
+                tvBottom.setText("");
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstNum = Double.parseDouble(tvBottom.getText().toString());
+                op = '/';
+                tvTop.setText(String.valueOf(firstNum) + '÷');
+                tvBottom.setText("");
+            }
+        });
+
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(tvBottom.getText().toString().length() > 0) {
+                    secondNum = Double.parseDouble(tvBottom.getText().toString());
+
+                    switch (op) {
+                        case '+':
+                            result = firstNum + secondNum;
+                            break;
+                        case '-':
+                            result = firstNum - secondNum;
+                            break;
+                        case '*':
+                            result = firstNum * secondNum;
+                            op = 'x';
+                            break;
+                        case '/':
+                            result = firstNum / secondNum;
+                            op = '÷';
+                            break;
+                    }
+
+                    if(rooted){
+                        firstNum = Double.parseDouble(tvBottom.getText().toString().substring(1));
+                        result = firstNum;
+                        String printSqrt = Double.toString(result);
+
+                        tvTop.setText("√" + printSqrt);
+                        tvBottom.setText(printSqrt);
+                    }else {
+                        tvTop.setText(Double.toString(firstNum)+op+Double.toString(secondNum));
+                        tvBottom.setText(Double.toString(result));
+                    }
+
+                }
+
+
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText("");
+                tvTop.setText("");
+            }
+        });
+
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = tvBottom.getText().toString();
+                if(tvBottom.length() > 0)
+                {
+                    String removeLastChar = input.substring(0, input.length()-1);
+                    tvBottom.setText(removeLastChar);
+                }
+            }
+        });
+
+        root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!tvBottom.getText().toString().contains("√")) {
+                    tvBottom.setText("√");
+                    rooted = true;
+                }
+            }
+        });
+
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!tvBottom.getText().toString().contains(".")) {
+                    tvBottom.setText(tvBottom.getText().toString()+'.');
+                }
+            }
+        });
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+0);
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+1);
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+2);
+            }
+        });
+
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+3);
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+4);
+            }
+        });
+
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+5);
+            }
+        });
+
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+6);
+            }
+        });
+
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+7);
+            }
+        });
+
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+8);
+            }
+        });
+
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvBottom.setText(tvBottom.getText().toString()+9);
+            }
+        });
+
+
+    }
+
+}
